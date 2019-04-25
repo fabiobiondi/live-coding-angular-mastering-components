cc96038463392ce4042b4958983dc51f6f784c2b
Fabio <info@fabiobiondi.com>
2019-04-25 02:36:55 +0200

    Add Animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     HttpClientModule,
     SharedModule
